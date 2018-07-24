1a8702c19e908e6a45117379863cb9288eedb513
datron <kartik1702@gmail.com>
2018-07-24 22:34:46 +0530

    First commit v2

---
+++ app/src/main/java/io/kirikcoders/bitcse/HomePagerAdapter.java
@@ -0,0 +1,33 @@
+package io.kirikcoders.bitcse;
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentStatePagerAdapter;
+
+import java.util.ArrayList;
+
+/**
+ * Created by Kartik on 24-Jul-18.
+ */
+public class HomePagerAdapter extends FragmentStatePagerAdapter {
+    private ArrayList<Fragment> fragmentArrayList = new ArrayList<>(5);
+
+    public HomePagerAdapter(FragmentManager fm) {
+        super(fm);
+    }
+
+    @Override
+    public Fragment getItem(int position) {
+        Fragment f = fragmentArrayList.get(position);
+        return f;
+    }
+
+    @Override
+    public int getCount() {
+        return 5;
+    }
+
+    public void addFragment(Fragment f){
+        fragmentArrayList.add(f);
+    }
+}
---
+++ app/src/main/java/io/kirikcoders/bitcse/MainActivity.java
@@ -0,0 +1,86 @@
+package io.kirikcoders.bitcse;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.MenuItem;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private ViewPager pager;
+    private HomePagerAdapter adapter;
+    private BottomNavigationView navigation;
+    private MenuItem prevMenuItem;
+    private ViewPager.OnPageChangeListener changeListener = new ViewPager.OnPageChangeListener() {
+        @Override
+        public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+
+        }
+
+        @Override
+        public void onPageSelected(int position) {
+            if (prevMenuItem != null) {
+                prevMenuItem.setChecked(false);
+            } else {
+                navigation.getMenu().getItem(0).setChecked(false);
+            }
+            Log.d("page", "onPageSelected: " + position);
+            navigation.getMenu().getItem(position).setChecked(true);
+            prevMenuItem = navigation.getMenu().getItem(position);
+
+        }
+
+        @Override
+        public void onPageScrollStateChanged(int state) {
+
+        }
+    };
+
+    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
+            = new BottomNavigationView.OnNavigationItemSelectedListener() {
+
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            switch (item.getItemId()) {
+                case R.id.navigation_events:
+                    pager.setCurrentItem(0);
+                    return true;
+                case R.id.navigation_tools:
+                    pager.setCurrentItem(1);
+                    return true;
+                case R.id.navigation_time_table:
+                    pager.setCurrentItem(2);
+                    return true;
+                case R.id.navigation_notifications:
+                    pager.setCurrentItem(3);
+                    return true;
+                case R.id.navigation_settings:
+                    pager.setCurrentItem(4);
+                    return true;
+            }
+            return false;
+        }
+    };
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        pager = findViewById(R.id.pager);
+        adapter = new HomePagerAdapter(getSupportFragmentManager());
+        adapter.addFragment(new EventFragment());
+        adapter.addFragment(new ToolsFragment());
+        adapter.addFragment(new TimeTableFragment());
+        adapter.addFragment(new NotificationsFragment());
+        adapter.addFragment(new SettingsFragment());
+        pager.setAdapter(adapter);
+        pager.addOnPageChangeListener(changeListener);
+        navigation = (BottomNavigationView) findViewById(R.id.navigation);
+        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+    }
+
+}
