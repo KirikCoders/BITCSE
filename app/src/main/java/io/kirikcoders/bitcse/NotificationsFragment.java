c90a7cdf6769d4ff8035213af572fc03d23ef4f1
datron <kartik54321@hotmail.com>
2019-01-08 16:51:45 +0530

    added spinner dropdown items

--- app/src/main/java/io/kirikcoders/bitcse/NotificationsFragment.java
+++ app/src/main/java/io/kirikcoders/bitcse/NotificationsFragment.java
@@ -4,6 +4,9 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+import android.widget.Toast;
 
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
@@ -12,10 +15,14 @@ import androidx.fragment.app.Fragment;
  * Created by Kartik on 24-Jul-18.
  */
 public class NotificationsFragment extends Fragment {
+    private Spinner notifSpinner;
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.fragment_notifications,container,false);
+        notifSpinner = rootView.findViewById(R.id.selectTypeOfNotif);
+        notifSpinner.setAdapter(ArrayAdapter.createFromResource(getContext(),R.array.notif_types,
+                android.R.layout.simple_spinner_dropdown_item));
         return rootView;
     }
 }
--- app/src/main/java/io/kirikcoders/bitcse/TimeTableFragment.java
+++ app/src/main/java/io/kirikcoders/bitcse/TimeTableFragment.java
@@ -1,9 +1,14 @@
 package io.kirikcoders.bitcse;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+import android.widget.Toast;
 
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
@@ -11,11 +16,37 @@ import androidx.fragment.app.Fragment;
 /**
  * Created by Kartik on 24-Jul-18.
  */
-public class TimeTableFragment extends Fragment {
+public class TimeTableFragment extends Fragment implements AdapterView.OnItemSelectedListener {
+    private Spinner semSpinner;
+    private Spinner sectionSpinner;
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.fragment_time_table,container,false);
+        semSpinner = rootView.findViewById(R.id.time_table_sem);
+        sectionSpinner = rootView.findViewById(R.id.time_table_section);
+        ArrayAdapter<CharSequence> semesterAdapter = ArrayAdapter.createFromResource(getContext(),R.array.semesters,
+                android.R.layout.simple_spinner_dropdown_item);
+        ArrayAdapter<CharSequence> sectionAdapter = ArrayAdapter.createFromResource(getContext(),R.array.sections,
+                android.R.layout.simple_spinner_dropdown_item);
+        semSpinner.setAdapter(semesterAdapter);
+        sectionSpinner.setAdapter(sectionAdapter);
+        semSpinner.setOnItemSelectedListener(this);
+        sectionSpinner.setOnItemSelectedListener(this);
         return rootView;
+    }
+
+    @Override
+    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+        Log.d("TimeTableFragment","Spinner");
+        Log.i("parent",parent.toString());
+        Log.i("view",view.toString());
+        Log.i("parent", String.valueOf(position));
+        Log.i("parent", String.valueOf(id));
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> parent) {
+
     }
 }
