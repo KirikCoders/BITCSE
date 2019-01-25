d5f8a28b9dab9ffde1bb3d93044aea51a6790b44
rottiakash <rottiakash@gmail.com>
2019-01-25 18:22:33 +0530

    Spinner added to Find Professor Activity

--- app/src/main/java/io/kirikcoders/bitcse/tools/FindProfessor/SearchFaculty.java
+++ app/src/main/java/io/kirikcoders/bitcse/tools/FindProfessor/SearchFaculty.java
@@ -4,14 +4,13 @@ import androidx.appcompat.app.AppCompatActivity;
 import io.kirikcoders.bitcse.R;
 import io.kirikcoders.bitcse.database.TimeTableDataBaseHelper;
 
-import android.content.Intent;
 import android.database.Cursor;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.ListView;
-import android.widget.TextView;
-import android.widget.Toast;
+import android.widget.Spinner;
 
 import java.io.IOException;
 /**
@@ -20,45 +19,162 @@ import java.io.IOException;
 
 public class SearchFaculty extends AppCompatActivity {
     String Daykey=new String();
+    int slot;
+    String resRoom;
+    String resSub;
+    String resSem;
+    String FacName=new String();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_search_faculty);
-        TextView tw=findViewById(R.id.facsearchtw);
-        tw.setText("Select Day:-");
-        TimeTableDataBaseHelper dataBaseHelper = new TimeTableDataBaseHelper(getApplicationContext());
+        Spinner day=findViewById(R.id.day_spinner);
+        Spinner Slot=findViewById(R.id.slot_spinner);
         ListView lst=findViewById(R.id.fac_search_lst);
         String Days[]={"Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"};
-        FacSearchModel adapter=new FacSearchModel(this,Days);
-        lst.setAdapter(adapter);
+        ArrayAdapter<String> Dayadapter=new ArrayAdapter<>(this,R.layout.support_simple_spinner_dropdown_item,Days);
+        Dayadapter.setDropDownViewResource(R.layout.support_simple_spinner_dropdown_item);
+        day.setAdapter(Dayadapter);
+        day.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                switch(position)
+                {
+                    case 0:Daykey="MON";
+                        break;
+                    case 1:Daykey="TUE";
+                        break;
+                    case 2:Daykey="WED";
+                        break;
+                    case 3:Daykey="THU";
+                        break;
+                    case 4:Daykey="FRI";
+                        break;
+                    case 5:Daykey="SAT";
+                        break;
+                }
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
+        TimeTableDataBaseHelper dataBaseHelper = new TimeTableDataBaseHelper(getApplicationContext());
         try {
             dataBaseHelper.createDatabase();
             dataBaseHelper.openDatabase();
+        } catch (IOException e) {
+            e.printStackTrace();}
+        Cursor c=dataBaseHelper.getSlot();
+        c.moveToFirst();
+        int num=c.getCount();
+        int i=1;
+        int j=0;
+        String[] slots=new String[num];
+        while(i<=num)
+        {
+            slots[j]=c.getString(c.getColumnIndex("timings"));
+            c.moveToNext();
+            i++;
+            j++;
         }
-        catch (IOException e) {
-            e.printStackTrace();
-        }
-
-        lst.setOnItemClickListener((parent, view, position, id) -> {
-            switch(position)
-            {
-                case 0:Daykey="MON";
-                       break;
-                case 1:Daykey="TUE";
-                    break;
-                case 2:Daykey="WED";
-                    break;
-                case 3:Daykey="THU";
-                    break;
-                case 4:Daykey="FRI";
-                    break;
-                case 5:Daykey="SAT";
-                    break;
+        ArrayAdapter<String> Slotadapter=new ArrayAdapter<>(this,R.layout.support_simple_spinner_dropdown_item,slots);
+        Slotadapter.setDropDownViewResource(R.layout.support_simple_spinner_dropdown_item);
+        Slot.setAdapter(Slotadapter);
+        Slot.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                switch(position)
+                {
+                    case 0:slot=1;
+                        break;
+                    case 1:slot=2;
+                        break;
+                    case 2:slot=3;
+                        break;
+                    case 3:slot=4;
+                        break;
+                    case 4:slot=5;
+                        break;
+                    case 5:slot=6;
+                        break;
+                    case 6:slot=7;
+                        break;
+                    case 7:slot=8;
+                        break;
+                    case 8:slot=9;
+                        break;
+                    case 9:slot=10;
+                        break;
+                    case 10:slot=11;
+                        break;
+                    case 11:slot=12;
+                        break;
+                    case 12:slot=13;
+                        break;
+                    case 13:slot=14;
+                        break;
+                    case 14:slot=15;
+                        break;
+                    case 15:slot=21;
+                        break;
+                    case 16:slot=22;
+                        break;
+                    case 17:slot=23;
+                        break;
+                }
             }
-            Intent i=new Intent(view.getContext(),SlotSelect.class);
-            i.putExtra("DAYKEY",Daykey);
-            startActivity(i);
 
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
         });
+        Cursor c1=dataBaseHelper.getnames();
+        c1.moveToFirst();
+        c1.moveToNext();
+        int num1=c1.getCount();
+        int i1=2;
+        int j1=0;
+        String[] names=new String[42];
+        while(i1<num1)
+        {
+            names[j1]=c1.getString(c1.getColumnIndex("name"));
+            c1.moveToNext();
+            i1++;
+            j1++;
+        }
+        FacSearchModel adapter=new FacSearchModel(this,names);
+        lst.setAdapter(adapter);
+        lst.setOnItemClickListener((parent, view, position, id) ->
+        {
+            c1.moveToFirst();
+            c1.moveToPosition(position+1);
+            FacName=c1.getString(c1.getColumnIndex("name"));
+            Cursor c2=dataBaseHelper.getdataName(FacName,Daykey,slot);
+            c2.moveToFirst();
+            try
+            {
+                resRoom = c2.getString(c2.getColumnIndex("room"));
+                resSub = c2.getString(c2.getColumnIndex("sub"));
+                resSem = c2.getString(c2.getColumnIndex("sem"));
+                OpenDialog_success();
+            }catch (Exception e)
+            {
+                OpenDialog_Failure();
+            }
+        });
+    }
+    public void OpenDialog_success()
+    {
+        ResDiag resDiag=new ResDiag(resRoom,resSub,resSem,1);
+        resDiag.show(getSupportFragmentManager(),"Success");
+    }
+
+    public void OpenDialog_Failure()
+    {
+        ResDiag resDiag=new ResDiag("null","null","null",0);
+        resDiag.show(getSupportFragmentManager(),"Failure");
     }
 }
